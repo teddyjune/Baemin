1ce4125887ff81c652ff5fe5f58788880b385b2a
yeonjoo.lee <teddyjune@naver.com>
2022-07-11 23:53:25 +0900

    할인 프로모션 배너 완성

--- lib/baemin_screen.dart
+++ lib/baemin_screen.dart
@@ -95,13 +95,47 @@ class BeaminScreen extends StatelessWidget {
             ),
           ],
         ),
-        ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Image.asset(
-              "assets/sale-banner.png",
-              height: 100,
-              fit: BoxFit.cover,
-            )),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 150,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.blue,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text("토스페이로 결제하면",
+                            style: TextStyle(color: Colors.white, fontSize: 17)),
+                        Text(
+                          "지금 바로",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text("1,500원 할인",
+                            style: TextStyle(
+                                color: Colors.yellow,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                  ),
+                ),
+                Image.asset("baemin-icon.png",
+                    width: 170, height: 150, fit: BoxFit.cover),
+                const SizedBox(width: 25),
+              ],
+            ),
+          ),
+        ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Card(
