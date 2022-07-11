e25a7408fd7f205917b68b160d41f5339af5e021
yeonjoo.lee <teddyjune@naver.com>
2022-07-11 16:21:25 +0900

    화면 틀 잡음

--- lib/baemin_screen.dart
+++ lib/baemin_screen.dart
@@ -12,7 +12,8 @@ class BeaminScreen extends StatelessWidget {
         leading: const Icon(Icons.apps_outlined),
         title: const Text("경기 안양시 동안구 동편로 1..."),
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications_none)),
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.notifications_none)),
           IconButton(onPressed: () {}, icon: const Icon(Icons.mood)),
         ],
       ),
@@ -20,11 +21,122 @@ class BeaminScreen extends StatelessWidget {
         children: [
           Container(
             decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10)),
               color: Color(0xff28C1BC),
             ),
             height: 56,
-          )
+            child: const Padding(
+              padding: EdgeInsets.fromLTRB(20, 0, 20, 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  hintText: '닭발? 순대? 곱창?',
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Flexible(
+                child: Card(
+                  child: Container(
+                    color: Colors.redAccent,
+                    height: 200,
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Card(
+                  child: Container(
+                    color: Colors.redAccent,
+                    height: 200,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Card(
+            child: Container(
+              color: Colors.yellow,
+              height: 100,
+            ),
+          ),
+          Row(
+            children: [
+              Flexible(
+                child: Card(
+                  child: Container(
+                    color: Colors.green,
+                    height: 100,
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Card(
+                  child: Container(
+                    color: Colors.green,
+                    height: 100,
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Card(
+                  child: Container(
+                    color: Colors.green,
+                    height: 100,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Card(
+            child: Container(
+              color: Colors.blue,
+              height: 150,
+            ),
+          ),
+          Row(
+            children: [
+              Flexible(
+                child: Card(
+                  child: Container(
+                    color: Colors.grey,
+                    height: 100,
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Card(
+                  child: Container(
+                    color: Colors.grey,
+                    height: 100,
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Card(
+                  child: Container(
+                    color: Colors.grey,
+                    height: 100,
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Card(
+                  child: Container(
+                    color: Colors.grey,
+                    height: 100,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
