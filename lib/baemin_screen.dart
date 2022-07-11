65d4a50bd8e875dd7f02dad4c3318d5b10242981
yeonjoo.lee <teddyjune@naver.com>
2022-07-11 19:58:15 +0900

    크기 조절 실패

--- lib/baemin_screen.dart
+++ lib/baemin_screen.dart
@@ -17,131 +17,130 @@ class BeaminScreen extends StatelessWidget {
           IconButton(onPressed: () {}, icon: const Icon(Icons.mood)),
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10)),
-              color: Color(0xff28C1BC),
-            ),
-            height: 56,
-            child: const Padding(
-              padding: EdgeInsets.fromLTRB(20, 0, 20, 15),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Colors.white,
-                  ),
-                  hintText: '닭발? 순대? 곱창?',
+      body: Column(children: [
+        Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10)),
+            color: Color(0xff28C1BC),
+          ),
+          height: 56,
+          child: const Padding(
+            padding: EdgeInsets.fromLTRB(20, 0, 20, 15),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.white,
                 ),
+                hintText: '닭발? 순대? 곱창?',
               ),
             ),
           ),
-          Row(
-            children: [
-              Flexible(
-                child: Card(
-                  child: Container(
-                    color: Colors.redAccent,
-                    height: 200,
-                  ),
+        ),
+        Row(children: [
+          Flexible(
+            child: Card(
+                semanticContainer: true,
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
                 ),
-              ),
-              Flexible(
-                child: Card(
-                  child: Container(
-                    color: Colors.redAccent,
-                    height: 200,
-                  ),
-                ),
-              ),
-            ],
+                child: Image.asset("assets/food1.png", fit: BoxFit.fill)),
           ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.yellow,
+          Flexible(
+            child: Card(
+                semanticContainer: true,
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Image.asset("assets/rider.png", fit: BoxFit.fill)),
+          ),
+        ]),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.yellow,
+          ),
+          height: 100,
+        ),
+        Row(
+          children: [
+            Flexible(
+              child: Card(
+                child: Container(
+                  color: Colors.green,
+                  height: 100,
+                ),
+              ),
             ),
-            height: 100,
-          ),
-          Row(
-            children: [
-              Flexible(
-                child: Card(
-                  child: Container(
-                    color: Colors.green,
-                    height: 100,
-                  ),
+            Flexible(
+              child: Card(
+                child: Container(
+                  color: Colors.green,
+                  height: 100,
                 ),
               ),
-              Flexible(
-                child: Card(
-                  child: Container(
-                    color: Colors.green,
-                    height: 100,
-                  ),
+            ),
+            Flexible(
+              child: Card(
+                child: Container(
+                  color: Colors.green,
+                  height: 100,
                 ),
               ),
-              Flexible(
-                child: Card(
-                  child: Container(
-                    color: Colors.green,
-                    height: 100,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          ClipRRect(
+            ),
+          ],
+        ),
+        Expanded(
+          child: ClipRRect(
               borderRadius: BorderRadius.circular(20.0),
               child: Image.asset(
                 "assets/sale-banner.png",
-                width: 470,
-                height: 160,
+                height: 100,
                 fit: BoxFit.cover,
               )),
-          Row(
-            children: [
-              Flexible(
-                child: Card(
-                  child: Container(
-                    color: Colors.grey,
-                    height: 100,
-                  ),
+        ),
+        Row(
+          children: [
+            Flexible(
+              child: Card(
+                child: Container(
+                  color: Colors.grey,
+                  height: 100,
                 ),
               ),
-              Flexible(
-                child: Card(
-                  child: Container(
-                    color: Colors.grey,
-                    height: 100,
-                  ),
+            ),
+            Flexible(
+              child: Card(
+                child: Container(
+                  color: Colors.grey,
+                  height: 100,
                 ),
               ),
-              Flexible(
-                child: Card(
-                  child: Container(
-                    color: Colors.grey,
-                    height: 100,
-                  ),
+            ),
+            Flexible(
+              child: Card(
+                child: Container(
+                  color: Colors.grey,
+                  height: 100,
                 ),
               ),
-              Flexible(
-                child: Card(
-                  child: Container(
-                    color: Colors.grey,
-                    height: 100,
-                  ),
+            ),
+            Flexible(
+              child: Card(
+                child: Container(
+                  color: Colors.grey,
+                  height: 100,
                 ),
               ),
-            ],
-          ),
-        ],
-      ),
+            ),
+          ],
+        ),
+      ]),
     );
   }
 }
